7a322745ba924ee862790f849caa4c2eba17b839
Brandon Walderman <brwalder@microsoft.com>
2022-04-06 12:13:50 -0700

    Add test.

---
+++ test.js
@@ -0,0 +1,17 @@
+const { Builder, By } = require('selenium-webdriver');
+
+(async () => {
+    const driver = await new Builder().forBrowser('MicrosoftEdge').build();
+    try {
+        await driver.get('https://bing.com');
+
+        const element = await driver.findElement(By.id('sb_form_q'));
+        await element.sendKeys('WebDriver');
+        await element.submit();
+
+        const title = await driver.getTitle();
+        console.log(title);
+    } finally {
+        await driver.quit();
+    }
+})();
